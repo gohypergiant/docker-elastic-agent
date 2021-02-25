a044d8509ef1679949756ba4998062b6271c0aa6
Bren Briggs <bbriggs@users.noreply.github.com>
2021-02-25 07:40:25 -0500

    Change how elastic-agent enrolls (#4)
    * Change how elastic-agent enrolls
    
    The combined install + enroll process is not producing healthy agents.
    Break it into two steps to allow for install, then enroll + run.
    
    * Fix job names

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-/opt/elastic/elastic-agent install -f --kibana-url=${KIBANA_URL} --enrollment-token=${ENROLLMENT_TOKEN}
-exec /opt/elastic/elastic-agent
+/opt/elastic/elastic-agent install
+exec /opt/elastic/elastic-agent enroll ${KIBANA_URL} ${ENROLLMENT_TOKEN}
