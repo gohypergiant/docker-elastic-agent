bffcc8d9bd038e78c2d1f10765dabf047a2a4f75
Bren Briggs <bbriggs@users.noreply.github.com>
2021-02-25 07:56:46 -0500

    Start then enroll agent (#5)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -x
 
 /opt/elastic/elastic-agent install
-exec /opt/elastic/elastic-agent enroll ${KIBANA_URL} ${ENROLLMENT_TOKEN}
+exec /opt/elastic/elastic-agent && /opt/elastic/elastic-agent enroll ${KIBANA_URL} ${ENROLLMENT_TOKEN}
